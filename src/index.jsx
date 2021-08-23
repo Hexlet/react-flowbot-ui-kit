12e95706f17f3fbf2d387b45b56207d6b313a558
Roman Makarov <deepspiral@yandex.ru>
2021-08-23 17:15:13 +0300

    init

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+// @ts-check
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/style.scss';
+
+const root = document.getElementById('root');
+
+const App = () => (
+  <div>Hello</div>
+);
+
+ReactDOM.render(<App />, root);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// @ts-check
+
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode,
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    port: 3000,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+};
