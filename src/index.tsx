26eccd705d4aed06c49cc376194fcae70837e86b
Ivan Gagarinov <dzencot@gmail.com>
2024-01-17 18:30:04 +0500

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,3 @@
-import { createRoot} from 'react-dom/client'
-import getWidget from './init.tsx';
-import steps from './steps.ts';
-import { Step } from './interfaces/Step.ts';
+import app from './app.tsx';
 
-const run = (): void => {
-  const widget = getWidget(steps as Step[]);
-  const container = document.getElementById('root');
-  if (container) {
-    const root = createRoot(container);
-    root.render(widget);
-  } else {
-    console.error('Root element not found');
-  }
-};
-
-run();
+app();
