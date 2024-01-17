5b3297994e14876a7a623e7e351f124ec313e2ed
ola-9 <nepogodda@gmail.com>
2024-01-17 15:56:54 +0300

    style ChatMessage Component

--- src/Components/ChatMessage.tsx
+++ src/Components/ChatMessage.tsx
@@ -7,11 +7,26 @@ interface ChatMessageProps {
 const ChatMessage = ({ contents }: ChatMessageProps) => {
   return (
     <div>      
-      {contents.map((items) => items.map((text, index) => (
-        <p key={index}>{text}</p>
-      )))}   
+      {contents.map((items, index) => (
+        <div key={index} className="d-flex">
+          <div className="avatar">
+            Здесь аватарка бота
+          </div>
+          <div className="d-flex flex-column">
+            {items.map((text, innerIndex) => (
+              <div
+                key={innerIndex}
+                className="rounded p-3 shadow mb-3"
+              >
+                {text}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}   
     </div>
   )
 }
+
 
 export default ChatMessage
