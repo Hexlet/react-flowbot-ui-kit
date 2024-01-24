c5bb2c8c7d86a00a037cea78b36b90d1fc81bb83
Elena Gromova <elena.gromova@hexlet.io>
2024-01-24 11:18:26 +0300

    removed extra imports

--- src/Components/WidgetModal.tsx
+++ src/Components/WidgetModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Chat from './Chat';
