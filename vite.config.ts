f612aed3a83ada7996d04bca24c2f6d397125cb2
Elena Gromova <elena.gromova@hexlet.io>
2024-02-07 19:13:54 +0300

    changed config for publishing as npm package

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,29 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     lib: {
-//       // Could also be a dictionary or array of multiple entry points
-//       entry: resolve(__dirname, 'src/app.tsx'),
-//       name: 'hexlet-flowbot',
-//       // the proper extensions will be added
-//       fileName: 'hexlet-flowbot',
-//     },
-//     rollupOptions: {
-//       external: ['react', 'react-dom'],
-//       output: {
-//         globals: {
-//           react: 'React',
-//           'react-dom': 'ReactDOM',
-//         },
-//       },
-//     },
-//   },
-// })
-
 export default defineConfig({
   plugins: [react()],
-  base: '/react-flowbot-ui-kit/'
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/app.tsx'),
+      name: 'hexlet-flowbot',
+      // the proper extensions will be added
+      fileName: 'hexlet-flowbot',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/react-flowbot-ui-kit/'
+// })
