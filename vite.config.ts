8e79ce9cdae64b0f9c11ecf41535110b1d8d4f7b
Elena Gromova <elena.gromova@hexlet.io>
2024-02-08 13:41:49 +0300

    fixed deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,31 +5,31 @@ import { resolve } from 'path';
 // for publishing as npm package
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/app.tsx'),
-      name: 'hexlet-flowbot',
-      // the proper extensions will be added
-      fileName: 'hexlet-flowbot',
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     lib: {
+//       // Could also be a dictionary or array of multiple entry points
+//       entry: resolve(__dirname, 'src/app.tsx'),
+//       name: 'hexlet-flowbot',
+//       // the proper extensions will be added
+//       fileName: 'hexlet-flowbot',
+//     },
+//     rollupOptions: {
+//       external: ['react', 'react-dom'],
+//       output: {
+//         globals: {
+//           react: 'React',
+//           'react-dom': 'ReactDOM',
+//         },
+//       },
+//     },
+//   },
+// })
 
 // for deploying on github pages
 
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/react-flowbot-ui-kit/'
-// })
+export default defineConfig({
+  plugins: [react()],
+  base: '/react-flowbot-ui-kit/'
+})
